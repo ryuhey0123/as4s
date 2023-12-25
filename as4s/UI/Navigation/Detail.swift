f581640cfdb652cb12c89f7080db1381f7d3e763
ryuhey0123 <34589715+ryuhey0123@users.noreply.github.com>
2023-12-26 00:46:35 +0900

    ref

--- as4s/UI/Navigation/Detail.swift
+++ as4s/UI/Navigation/Detail.swift
@@ -11,42 +11,10 @@ import SplitView
 struct Detail: View {
     @EnvironmentObject var store: Store
     
-    @State private var showingAccesary: Bool = false
-    @State private var currentHeight: CGFloat = 200
-    @State private var storeHeight: CGFloat = 200
-
     var body: some View {
             ZStack(alignment: .bottom) {
                 ModelView()
-                
-                GeometryReader { geometry in
-                    VStack {
-                        PrimaryInfoToolbar(showingAccesary: $showingAccesary)
-                            .position(x: geometry.frame(in: .local).width / 2, y: currentHeight)
-                            .gesture(
-                                DragGesture()
-                                    .onChanged { gesture in
-                                        currentHeight = gesture.location.y
-                                    }
-                            )
-                            .zIndex(2.0)
-                            .onChange(of: showingAccesary) {
-                                withAnimation {
-                                    if showingAccesary {
-                                        currentHeight = storeHeight
-                                    } else {
-                                        storeHeight = currentHeight
-                                        currentHeight = geometry.frame(in: .local).height - 12.5
-                                    }
-                                }
-                            }
-                        
-                        TextinfoView(output: $store.openSeesStdErr, input: $store.openSeesInput)
-                            .frame(height: geometry.frame(in: .local).height - currentHeight - 12.5)
-                            .background(.ultraThinMaterial)
-                            .zIndex(1.0)
-                    }
-            }
+                InformationPanel()
         }
     }
 }
--- as4s/UI/Views/InfoView/InformationPanel.swift
+++ as4s/UI/Views/InfoView/InformationPanel.swift
@@ -21,11 +21,14 @@ struct InformationPanel: View {
         GeometryReader { geometry in
             VStack {
                 PrimaryInfoToolbar(showingAccesary: $showingAccesary)
-                    .position(x: geometry.frame(in: .local).width / 2, y: currentPosition)
+                    .position(x: geometry.frame(in: .global).width / 2, y: currentPosition)
                     .gesture(
                         DragGesture()
                             .onChanged { gesture in
-                                currentPosition = min(max(100, gesture.location.y), geometry.frame(in: .local).height - 100)
+                                withAnimation(.linear(duration: 0.1)) {
+                                    currentPosition = min(max(100, gesture.location.y), geometry.frame(in: .global).height - 100)
+                                }
+                                showingAccesary = true
                             }
                     )
                     .onChange(of: showingAccesary) {
@@ -34,19 +37,19 @@ struct InformationPanel: View {
                                 currentPosition = storePosition
                             } else {
                                 storePosition = currentPosition
-                                currentPosition = geometry.frame(in: .local).height - subToolBarHeightHalf
+                                currentPosition = geometry.frame(in: .global).height - subToolBarHeightHalf
                             }
                         }
                     }
                     .zIndex(2)
                 
                 TextinfoView(output: $store.openSeesStdErr, input: $store.openSeesInput)
-                    .frame(height: geometry.frame(in: .local).height - currentPosition - subToolBarHeightHalf)
+                    .frame(height: max(0, geometry.frame(in: .global).height - currentPosition - subToolBarHeightHalf))
                     .background(.ultraThinMaterial)
                     .zIndex(1)
             }
             .onAppear {
-                currentPosition = showingAccesary ? 200 : geometry.frame(in: .local).height - subToolBarHeightHalf
+                currentPosition = showingAccesary ? 200 : geometry.frame(in: .global).height - subToolBarHeightHalf
             }
         }
     }
