6ce037bcd5dae45f98901e76932eb2c9562f45a0
ryuhey0123 <34589715+ryuhey0123@users.noreply.github.com>
2023-12-26 22:20:33 +0900

    ref

--- as4s/UI/Views/InfoView/InformationPanel.swift
+++ as4s/UI/Views/InfoView/InformationPanel.swift
@@ -15,58 +15,106 @@ struct InformationPanel: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                PrimaryInfoToolbarView(geometry: geometry, currentHeight: $currentHeight)
+                ResizableToolbar(geometry: geometry, currentHeight: $currentHeight)
                     .zIndex(1)
-                TextinfoView(output: $store.openSeesStdErr, input: $store.openSeesInput)
+                Textinfo(output: $store.openSeesStdErr, input: $store.openSeesInput)
                     .frame(height: currentHeight)
             }
             
         }
     }
-}
-
-fileprivate struct PrimaryInfoToolbarView: View {
-    var geometry: GeometryProxy
-    @Binding var currentHeight: CGFloat
     
-    @State private var showingAccesary: Bool = false
-    @State private var storedHeight: CGFloat = 200
-    
-    let subToolBarHeightHalf: CGFloat = 12.5
-    let minTopHeight: CGFloat = 100
-    let minBottomHeight: CGFloat = 100
-    
-    var body: some View {
-        PrimaryInfoToolbar(showingAccesary: $showingAccesary)
-            .position(x: geometry.frame(in: .global).width / 2,
-                      y: geometry.frame(in: .global).height - currentHeight - subToolBarHeightHalf)
-            .gesture(
-                DragGesture()
-                    .onChanged { gesture in
-                        withAnimation(.linear(duration: 0.1)) {
-                            let height = geometry.frame(in: .global).height
-                            currentHeight = clamp(value: height - gesture.location.y,
-                                                  in: minTopHeight...height - minBottomHeight)
+    struct ResizableToolbar: View {
+        var geometry: GeometryProxy
+        @Binding var currentHeight: CGFloat
+        
+        @State private var showingAccesary: Bool = false
+        @State private var storedHeight: CGFloat = 200
+        
+        let subToolBarHeightHalf: CGFloat = 12.5
+        let minTopHeight: CGFloat = 100
+        let minBottomHeight: CGFloat = 100
+        
+        var body: some View {
+            PrimaryInfoToolbar(showingAccesary: $showingAccesary)
+                .position(x: geometry.frame(in: .global).width / 2,
+                          y: geometry.frame(in: .global).height - currentHeight - subToolBarHeightHalf)
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            withAnimation(.linear(duration: 0.1)) {
+                                let height = geometry.frame(in: .global).height
+                                currentHeight = clamp(value: height - gesture.location.y,
+                                                      in: minTopHeight...height - minBottomHeight)
+                            }
+                            showingAccesary = true
                         }
-                        showingAccesary = true
-                    }
-            )
-            .onChange(of: showingAccesary) {
-                withAnimation(.easeInOut(duration: 0.2)) {
-                    if showingAccesary {
-                        currentHeight = storedHeight
-                    } else {
-                        storedHeight = currentHeight
-                        currentHeight = 0
+                )
+                .onChange(of: showingAccesary) {
+                    withAnimation(.easeInOut(duration: 0.2)) {
+                        if showingAccesary {
+                            currentHeight = storedHeight
+                        } else {
+                            storedHeight = currentHeight
+                            currentHeight = 0
+                        }
                     }
                 }
-            }
+        }
+        
+        func clamp(value: CGFloat, in range: ClosedRange<CGFloat>) -> CGFloat {
+            min(max(value, range.lowerBound), range.upperBound)
+        }
     }
     
-    private func clamp(value: CGFloat, in range: ClosedRange<CGFloat>) -> CGFloat {
-        min(max(value, range.lowerBound), range.upperBound)
+    struct PrimaryInfoToolbar: View {
+        @Binding var showingAccesary: Bool
+        
+        var body: some View {
+            VStack(spacing: -1.0) {
+                ZStack {
+                    HStack {
+                        Text("Hello!")
+                            .foregroundStyle(.secondary)
+                        Spacer()
+                        Toggle(isOn: $showingAccesary, label: {
+                            Label("Show Information", systemImage: "square.bottomthird.inset.filled")
+                                .labelStyle(.iconOnly)
+                        })
+                        .toggleStyle(.button)
+                        .buttonStyle(.borderless)
+                    }
+                    .padding(.horizontal)
+                    .frame(height: 25)
+                    .contentShape(
+                        Rectangle()
+                    )
+                    
+                    HStack {
+                        Spacer()
+                            .frame(width: 100)
+                        Rectangle()
+                            .fill(.clear)
+                            .onHover { hovering in
+                                if hovering {
+                                    NSCursor.resizeUpDown.push()
+                                } else {
+                                    NSCursor.pop()
+                                }
+                            }
+                        Spacer()
+                            .frame(width: 100)
+                    }
+                    .frame(height: 25)
+                }
+                Divider()
+            }
+            .background(.background)
+        }
     }
 }
+
+
 
 #Preview {
     struct PreviewWrapper: View {
@@ -81,6 +129,17 @@ fileprivate struct PrimaryInfoToolbarView: View {
                     .border(.blue)
                     .environmentObject(Store.debug)
             }
+        }
+    }
+    return PreviewWrapper()
+}
+
+#Preview("Toolbar") {
+    struct PreviewWrapper: View {
+        @State var showingAccesary: Bool = false
+        
+        var body: some View {
+            InformationPanel.PrimaryInfoToolbar(showingAccesary: $showingAccesary)
         }
     }
     return PreviewWrapper()
---
+++ as4s/UI/Views/InfoView/Textinfo.swift
@@ -0,0 +1,146 @@
+//
+//  Textinfo.swift
+//  as4s
+//
+//  Created by Ryuhei Fujita on 2023/12/22.
+//
+
+import SwiftUI
+import HighlightedTextEditor
+
+struct Textinfo: View {
+    @Binding var output: String
+    @Binding var input: String
+    
+    @State private var showingOutput: Bool = true
+    @State private var showingInput: Bool = true
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            HStack(spacing: -1.0) {
+                if showingOutput {
+                    OutputText(text: $output)
+                        .transition(.move(edge: .leading))
+                }
+                Divider()
+                if showingInput {
+                    InputText(text: $input)
+                        .transition(.move(edge: .trailing))
+                }
+            }
+            .animation(.interactiveSpring, value: showingOutput)
+            .animation(.interactiveSpring, value: showingInput)
+            
+            Divider()
+            
+            Toolbar(showingOutput: $showingOutput, showingInput: $showingInput)
+                .background(.ultraThinMaterial)
+        }
+        .background(.background)
+    }
+    
+    struct InputText: View {
+        @Binding var text: String
+        
+        var body: some View {
+            VStack {
+                HighlightedTextEditor(text: $text, highlightRules: OpenSeesHighlightRule.rules)
+                    .introspect { editor in
+                        editor.scrollView?.drawsBackground = false
+                        editor.textView.drawsBackground = false
+                    }
+            }
+        }
+    }
+    
+    struct OutputText: View {
+        @Binding var text: String
+        
+        @State private var showLabel: Bool = true
+        
+        var body: some View {
+            ScrollView(.vertical) {
+                ZStack {
+                    Text(text)
+                        .font(.callout)
+                        .fontDesign(.monospaced)
+                        .multilineTextAlignment(.leading)
+                        .textSelection(.enabled)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                        .padding(.leading)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    struct Toolbar: View {
+        @Binding var showingOutput: Bool
+        @Binding var showingInput: Bool
+        
+        var body: some View {
+            HStack {
+                Spacer()
+                Toggle(isOn: $showingOutput, label: {
+                    Label("Show Output", systemImage: "square.leftthird.inset.filled")
+                        .labelStyle(.iconOnly)
+                })
+                .toggleStyle(.button)
+                .buttonStyle(.borderless)
+                .onChange(of: showingOutput) {
+                    if !showingOutput && !showingInput {
+                        showingInput = true
+                        showingOutput = true
+                    }
+                }
+                
+                Toggle(isOn: $showingInput, label: {
+                    Label("Show Input", systemImage: "square.rightthird.inset.filled")
+                        .labelStyle(.iconOnly)
+                })
+                .toggleStyle(.button)
+                .buttonStyle(.borderless)
+                .onChange(of: showingInput) {
+                    if !showingOutput && !showingInput {
+                        showingInput = true
+                        showingOutput = true
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .frame(height: 25)
+        }
+    }
+}
+
+#Preview {
+    Textinfo(output: .constant(SampleText.output), input: .constant(SampleText.input))
+        .frame(width: 600)
+}
+
+#Preview("Input") {
+    Textinfo.OutputText(text: .constant(SampleText.output))
+}
+
+#Preview("Output") {
+    struct PreviewWrapper: View {
+        @State var text: String = SampleText.input
+
+        var body: some View {
+            Textinfo.InputText(text: $text)
+        }
+    }
+    return PreviewWrapper()
+}
+
+#Preview("Toolbar") {
+    struct PreviewWrapper: View {
+        @State var showingOutput: Bool = false
+        @State var showingInput: Bool = false
+        
+        var body: some View {
+            Textinfo.Toolbar(showingOutput: $showingOutput, showingInput: $showingInput)
+        }
+    }
+    return PreviewWrapper()
+}
